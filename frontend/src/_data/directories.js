5b0afbf8cc5594e789716a2d6b8c796acdbb09e1
joh-sch <johannes@schmoll.studio>
2026-02-08 12:47:39 +0100

    temporarily disable publish filter in directory data processing due to "publish" field currently not being included in item data

--- frontend/src/_data/directories.js
+++ frontend/src/_data/directories.js
@@ -83,7 +83,8 @@ module.exports = async function () {
                 });
 
                 // Filter items based on publish status
-                const publishedItems = allItems.filter((item) => isPublished(item[publishField]));
+                // const publishedItems = allItems.filter((item) => isPublished(item[publishField]));
+                const publishedItems = allItems; // temporary disabling publish filter as field is not being included
 
                 // Warn if all items were filtered out (likely configuration issue)
                 if (allItems.length > 0 && publishedItems.length === 0) {
